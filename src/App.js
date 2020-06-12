d58a631be2d65ee6f3c806f7e479063e2ffcbd46
camdenl <camden@rogoag.com>
2020-06-12 15:22:56 -0400

    finished download and render from airtable

--- src/App.js
+++ src/App.js
@@ -6,32 +6,61 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      jobs: {}
+      records: [],
+      isLoaded: false,
+      error: false
     };
   }
   componentDidMount() {
-    const jobs = {}; 
-    base('Jobs').select({
-      view: "camden_dev"
-    }).eachPage(function page(records, fetchNextPage) {
-      console.log(records)
-      records.forEach(function (record) {
-        const jobName = record.get('Name');
-        const jobID = record['id'];
-        jobs[jobID] = jobName;
+    const TABLE_NAME = 'Jobs';
+    const VIEW_NAME = 'camden_dev'
+    const table = base(TABLE_NAME)
+    const jobs = [];
+    const getData = view => {
+      return table.select({
+        view: view
+      }).all()
+    }
+    getData(VIEW_NAME)
+      .then((resp) => {
+        resp.forEach((record) => {
+          const jobName = record.get('Name');
+          const jobID = record['id'];
+          jobs.push({
+            'key': jobID, 'name': jobName
+          });
+        });
+        this.setState({
+          records: jobs,
+          isLoaded: true,
+          error: false
+        });
+        console.log(jobs);
+      })
+      .catch((error) => {
+        console.log(error);
+        this.setState({ error: true });
       });
-      fetchNextPage();
-    }, function done(err) {
-      if (err) { 
-        console.error(err); 
-      }
-    });
-    this.setState({records: jobs});
   }
   render() {
     return (
       <div>
-        <div>Hello</div>
+        {
+          // if the data loaded correctly
+          (this.state.isLoaded && !this.state.error) ? (
+            this.state.records.map(record =>
+              <li key={record.key}>
+                {record.name}
+              </li>
+            )
+          ) :
+            // else if there is no error, it is loading
+            (!this.state.error) ? (
+              <p>Loading...</p>
+            ) :
+              // else error
+              (<p>error</p>)
+        }
       </div>
     )
   }
